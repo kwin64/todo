b455a691c8385348240ce874db8df37a741d007a
Eugene <https://github.com/kwin64/todo.git>
2021-03-02 23:44:28 +0300

    todo added

---
+++ todo/src/Todolist.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function TodoList() {
+    return (
+        <div>
+            <h3>title</h3>
+            <input />
+            <button>X</button>
+            <ul>
+                <li>task_1</li>
+                <li>task_2</li>
+                <li>task_3</li>
+                <li>task_4</li>
+                <li>task_5</li>
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
--- todo/src/index.tsx
+++ todo/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoList from './Todolist';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
