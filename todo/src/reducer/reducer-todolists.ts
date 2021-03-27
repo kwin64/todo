fc5fd6ed41eb8e248b28360284a80764c74c9c3c
kwin11bvb@yandex.by <kwin11bvb@yandex.by>
2021-03-27 21:28:25 +0300

    todolist reducer

--- todo/src/reducer/reducer-todolists.ts
+++ todo/src/reducer/reducer-todolists.ts
@@ -1,27 +1,50 @@
 import React from 'react';
 import {v1} from 'uuid';
-import {TodolistType} from "../App";
+import {FilterValueType, TodolistType} from "../App";
 
-type AddTodolistACType = {
+type AddTodolistActionType = {
     type: 'ADD-TODOLIST'
     title: string
 }
-type RemoVetodolistACType = {
+type RemoVetodolistActionType = {
     type: 'REMOVE-TODOLIST'
     todoListsID: string
 }
+type ChangeTodolistFilterActionType = {
+    type: 'CHANGE-TODOLIST-FILTER'
+    filter: FilterValueType
+    id: string
+}
+type ChangeTodolistTitleActionType = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    title: string
+    id: string
+}
 
-type ActionsType = AddTodolistACType | RemoVetodolistACType
+type ActionsType = AddTodolistActionType
+    | RemoVetodolistActionType
+    | ChangeTodolistFilterActionType
+    | ChangeTodolistTitleActionType
 
-export const useReducer = (state: Array<TodolistType>, action: ActionsType) => {
+export const useReducer = (state: Array<TodolistType>, action: ActionsType): Array<TodolistType> => {
     switch (action.type) {
         case 'ADD-TODOLIST':
-            const idTodoList = v1()
-            const todo: TodolistType = {id: idTodoList, title: action.title, filter: "all"}
-            const copyState = [...state, todo]
-            return copyState
+            return [...state, {id: v1(), title: action.title, filter: "all"}]
         case 'REMOVE-TODOLIST':
-            
+            return state.filter(tl => tl.id !== action.todoListsID)
+        case 'CHANGE-TODOLIST-FILTER':
+            const todoList = state.find(tl => tl.id === action.id)
+            if (todoList) {
+                todoList.filter = action.filter
+            }
+            return [...state]
+        case 'CHANGE-TODOLIST-TITLE': {
+            const todoList = state.find(tl => tl.id === action.id)
+            if (todoList) {
+                todoList.title = action.title
+            }
+            return [...state]
+        }
         default:
             throw new Error("don't understand this type")
     }
