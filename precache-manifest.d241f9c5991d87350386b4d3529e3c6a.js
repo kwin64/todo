b352bc878a5387dd5359ede6beabd2d6df2dbe92
kwin64 <kwin11bvb@yandex.by>
2021-06-03 20:34:49 +0300

    Updates

---
+++ precache-manifest.d241f9c5991d87350386b4d3529e3c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f6080d8f71c86bb814cf0d0e5a2cc6",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "96e9df4c36b169918e7a",
+    "url": "/todo/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/todo/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e9df4c36b169918e7a",
+    "url": "/todo/static/js/main.68901f9f.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
